dd7c1c64e5534b02640c5265ce3a4a4be5f2ac62
Shubhamkumar619 <100133975+Shubhamkumar619@users.noreply.github.com>
2022-03-07 00:51:42 +0530

    Add files via upload

---
+++ employee-project-two-master/bin/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee(id,name,designation,salary,age) values(101,'Shubham','Manager',120000,24);
+INSERT INTO employee(id,name,designation,salary,age) values(102,'Vikas','CEO',1200000,22);
+INSERT INTO employee(id,name,designation,salary,age) values(103,'Amit','Developer',1200000,28);
+INSERT INTO employee(id,name,designation,salary,age) values(104,'Kartik','CFO',3600000,24);
+
+INSERT INTO employee(id,name,designation,salary,age) values(105,'Shubham','Manager',400,24);
+INSERT INTO employee(id,name,designation,salary,age) values(106,'Vikas','CEO',410,99);
+INSERT INTO employee(id,name,designation,salary,age) values(107,'Amit','Developer',420,182);
+INSERT INTO employee(id,name,designation,salary,age) values(108,'Kartik','CFO',430,69);
